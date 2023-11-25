a10b1087bd02afd4e76385f9df0f3259dc370bc5
ale87jan <50618873+ale87jan@users.noreply.github.com>
2023-11-25 20:37:03 +0100

    v1 que permite tramming y autonivelado

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2097,9 +2097,10 @@
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
-//#define LCD_BED_TRAMMING
+#define LCD_BED_TRAMMING
 
 #if ENABLED(LCD_BED_TRAMMING)
+<<<<<<< HEAD
 <<<<<<< HEAD
   #define BED_TRAMMING_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
   #define BED_TRAMMING_HEIGHT      0.0        // (mm) Z height of nozzle at tramming points
@@ -2108,6 +2109,9 @@
   //#define BED_TRAMMING_USE_PROBE
 =======
   #define BED_TRAMMING_INSET_LFRB { 20, 20, 20, 20 } // (mm) Left, Front, Right, Back insets
+=======
+  #define BED_TRAMMING_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
+>>>>>>> 362e5b6e18 (v1 que permite tramming y autonivelado)
   #define BED_TRAMMING_HEIGHT 0.0             // (mm) Z height of nozzle at leveling points
   #define BED_TRAMMING_Z_HOP 4.0              // (mm) Z height of nozzle between leveling points
   #define BED_TRAMMING_INCLUDE_CENTER       // Move to the center after the last corner
