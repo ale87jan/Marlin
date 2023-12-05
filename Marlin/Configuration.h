5e84931e525f5d6a4dd603c9a9ad299819862d4f
ale87jan <50618873+ale87jan@users.noreply.github.com>
2023-12-05 15:57:26 +0100

    Reajuste offset y velocidad de cambio filamento de mm/s a mm/min

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1541,7 +1541,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, 0 } //CAMBIAR AQUI
+#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -0.52 } //CAMBIAR AQUI
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
