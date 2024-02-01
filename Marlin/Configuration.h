a1b9209abe2994c52505d9cbd1385460fdd69d23
ale87jan <50618873+ale87jan@users.noreply.github.com>
2024-02-01 19:59:00 +0100

    Updated PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -670,9 +670,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp 24.87  //CAMBIAR AQUI
-    #define DEFAULT_Ki 2.33   //CAMBIAR AQUI
-    #define DEFAULT_Kd 66.38  //CAMBIAR AQUI
+    #define DEFAULT_Kp 24.45  //CAMBIAR AQUI
+    #define DEFAULT_Ki 2.30   //CAMBIAR AQUI
+    #define DEFAULT_Kd 64.82  //CAMBIAR AQUI
   #endif
 #endif
 
