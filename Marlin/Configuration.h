df2e98cc3e60cd9fd4eebf6dd9ed8e8ebb22bdc1
ale87jan <50618873+ale87jan@users.noreply.github.com>
2024-02-04 13:56:54 +0100

    actualizada junction deviation y aceleraciones por defecto

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1266,8 +1266,12 @@
 =======
 #define DEFAULT_ACCELERATION 500              // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION 500      // E acceleration for retracts
+<<<<<<< HEAD
 #define DEFAULT_TRAVEL_ACCELERATION 500       // X, Y, Z acceleration for travel (non printing) moves
 >>>>>>> ea266fe83d (v1)
+=======
+#define DEFAULT_TRAVEL_ACCELERATION 1000       // X, Y, Z acceleration for travel (non printing) moves
+>>>>>>> 1a27dbb9cb (actualizada junction deviation y aceleraciones por defecto)
 
 /**
  * Default Jerk limits (mm/s)
@@ -1307,7 +1311,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.2 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
