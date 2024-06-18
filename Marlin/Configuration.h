4e46e9ed13a1bf37445a4956ff5bd40b9fb80f5a
ale87jan <50618873+ale87jan@users.noreply.github.com>
2024-06-18 19:20:05 +0200

    commit tras puesta al dia en junio 24

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2222,10 +2222,10 @@
 
 #if ENABLED(LCD_BED_TRAMMING)
   #define BED_TRAMMING_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
-  #define BED_TRAMMING_HEIGHT      0.0        // (mm) Z height of nozzle at tramming points
-  #define BED_TRAMMING_Z_HOP       4.0        // (mm) Z raise between tramming points
-  //#define BED_TRAMMING_INCLUDE_CENTER       // Move to the center after the last corner
-  //#define BED_TRAMMING_USE_PROBE
+  #define BED_TRAMMING_HEIGHT 0.0             // (mm) Z height of nozzle at tramming points
+  #define BED_TRAMMING_Z_HOP 4.0              // (mm) Z raise between tramming points
+  #define BED_TRAMMING_INCLUDE_CENTER       // Move to the center after the last corner
+  #define BED_TRAMMING_USE_PROBE
   #if ENABLED(BED_TRAMMING_USE_PROBE)
     #define BED_TRAMMING_PROBE_TOLERANCE 0.05  // (mm)
     #define BED_TRAMMING_VERIFY_RAISED        // After adjustment triggers the probe, re-probe to verify
